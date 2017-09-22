560cdccf54584ea90cb36e0e6a9260ab558f3e1f
Julien Portalier <julien@portalier.com>
2017-09-22 18:45:00 +0200

    Sample: debug C or C++ AST

--- samples/debug.cr
+++ samples/debug.cr
@@ -16,6 +16,23 @@ def visit(parent, deep = 0)
 
     puts "#{cursor.kind}: spelling=#{cursor.spelling} type.kind=#{cursor.type.kind} type.spelling=#{cursor.type.spelling.inspect} at #{cursor.location}"
 
+    case cursor.kind
+    when .class_decl?
+      puts [:class, cursor.type.size_of].inspect
+    when .field_decl?
+      puts [:field, cursor.offset_of_field].inspect
+    when .function_decl?
+      puts [:function, cursor.mangling].inspect
+    when .constructor?
+      puts [:constructor, cursor.cxx_manglings].inspect
+    when .destructor?
+      puts [:destructor, cursor.cxx_manglings].inspect
+    #when .cxx_method?
+    #  puts [:cxx_method, cursor.spelling].inspect
+    when .cxx_access_specifier?
+      puts [:cxx_access_specifier, cursor.cxx_access_specifier].inspect
+    end
+
     visit(cursor, deep + 2)
 
     Clang::ChildVisitResult::Continue
@@ -25,16 +42,26 @@ end
 index = Clang::Index.new
 
 file_name = ARGV[0]? || "clang-c/Documentation.h"
-files = [
-  Clang::UnsavedFile.new("input.c", "#include <#{file_name}>\n")
-]
 args = [
   "-I/usr/include",
-  "-I/usr/lib/llvm-3.9/include",
+  "-I/usr/lib/llvm-5.0/lib/clang/5.0.0/include",
+  "-I/usr/lib/llvm-5.0/include",
 ]
 options = Clang::TranslationUnit.default_options |
-  Clang::TranslationUnit::Options::DetailedPreprocessingRecord
+  Clang::TranslationUnit::Options::DetailedPreprocessingRecord |
+  Clang::TranslationUnit::Options::SkipFunctionBodies
 
-tu = Clang::TranslationUnit.from_source(index, files, args, options)
+case File.extname(file_name)
+when ".h"
+  files = [Clang::UnsavedFile.new("input.c", "#include <#{file_name}>\n")]
+  tu = Clang::TranslationUnit.from_source(index, files, args, options)
+when ".hpp"
+  files = [Clang::UnsavedFile.new("input.cpp", "#include <#{file_name}>\n")]
+  tu = Clang::TranslationUnit.from_source(index, files, args, options)
+else
+  #tu = Clang::TranslationUnit.from_source_file(index, file_name, args)
+  files = [Clang::UnsavedFile.new(file_name, File.read(file_name))]
+  tu = Clang::TranslationUnit.from_source(index, files, args, options)
+end
 
 visit(tu.cursor)
